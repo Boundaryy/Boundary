e56cd3013565fa9e5a6afaab2c47567667761f8e
huise0ng <shinhuiseong07@gmail.com>
2024-08-06 23:33:35 +0900

    FIX :: 경로 및 스타일

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,37 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9'),
+	() => import('./nodes/10')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/auth/signup": [3],
+		"/auth/signup/info": [4],
+		"/home": [5],
+		"/mypage": [6],
+		"/training/feedback": [7],
+		"/training/resolve": [8],
+		"/training/succes": [9],
+		"/training/sudoku": [10]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
