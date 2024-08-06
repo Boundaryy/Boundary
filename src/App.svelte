3615ceda70c77e23c7d28b66cd3287bb3b8fe3fa
huise0ng <shinhuiseong07@gmail.com>
2024-08-06 20:13:10 +0900

    ADD :: 중간점검

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
-	import { Router, Route, Link } from "svelte-routing";
-	import Landing from './pages/sign/Landing.svelte';
-	import Signup from './pages/sign/Signup.svelte';
-	
-	export let url = "";
-  </script>
-  
-  <main>
-	<Router {url}>
-	  <Route path="/" component={Landing} />
-	  <Route path="/signup" component={Signup} />
-	</Router>
-  </main>
+    import { Router, Route, Link, navigate } from "svelte-routing";
+    import Landing from './pages/sign/Landing.svelte';
+    import Signup from './pages/sign/Signup.svelte';
+    import Signup2 from './pages/sign/Signuptwo.svelte';
+
+    export let url = "";
+</script>
+
+<main>
+    <Router {url}>
+        <Route path="/" component={Landing} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signup2" component={Signup2} />
+    </Router>
+</main>
